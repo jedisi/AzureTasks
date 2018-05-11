bb5a62e7008a08b5e1a304dcb35c1836dbf433d6
Aliaksandr Simanau <Aliaksandr_Simanau@epam.com>
2018-05-11 11:25:04 +0300

    updated param file

--- 3/deploy.ps1
+++ 3/deploy.ps1
@@ -1,6 +1,6 @@
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 $ResourceGroupName = "Task3"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location westeurope -Verbose
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName 
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile "C:\Users\Aliaksandr_Simanau\Desktop\Azure\Git\AzureTasks\3\main.json" -TemplateParameterUri "https://raw.githubusercontent.com/jedisi/AzureTasks/master/3/param.json"
