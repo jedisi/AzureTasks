bd213996dcd543ffa02c190cad3a41d3a4c6ccff
Aliaksandr Simanau <Aliaksandr_Simanau@epam.com>
2018-05-11 14:55:43 +0300

    Added NSG block

--- 3/deploy.ps1
+++ 3/deploy.ps1
@@ -3,4 +3,6 @@
 $ResourceGroupName = "Task3"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location westeurope -Verbose
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri "https://raw.githubusercontent.com/jedisi/AzureTasks/master/3/main.json" -TemplateParameterUri "https://raw.githubusercontent.com/jedisi/AzureTasks/master/3/param.json" -Debug
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                   -TemplateUri "https://raw.githubusercontent.com/jedisi/AzureTasks/master/3/main.json" `
+                                   -TemplateParameterUri "https://raw.githubusercontent.com/jedisi/AzureTasks/master/3/param.json" -Debug
